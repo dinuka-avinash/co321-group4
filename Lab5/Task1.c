3a907a14cc80310e753351cf470c982b2cd61dff
Dinuka Avinash <115539642+dinuka-avinash@users.noreply.github.com>
2024-01-24 10:48:47 +0530

    Lab 5 completed

--- Lab5/Task1.c
+++ Lab5/Task1.c
@@ -3,26 +3,15 @@
 #include <util/delay.h>
 
 void adc_init() {
-    // DDRC &= ~(1 << PC1);
 
     ADCSRA |= (1 << ADEN);    // Enable the ADC
-    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADATE);    // Set 64 as the conversion speed
-    // ADCSRA |= (1 << ADATE);    
+
+    ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADATE);    // Set 64 as the conversion speed  
 
     ADMUX |= (1 << REFS0) | (1 << ADLAR) | (1 << MUX0);  // Set voltage reference as AVCC, left adjust result, and set ADC1 as the analog input channel
-    UBRR0 = 103;
 
-    // Enable USART transmitter and receiver
-    UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-
-    // 8-bit data, no parity, 1 stop bit
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-void usart_send(int data) {    
-    while (!(UCSR0A & (1 << UDRE0)));   // Wait until the transmitter is ready
-    UDR0 = data;
-}
 
 
 int main() {
@@ -31,33 +20,25 @@ int main() {
     adc_init();
 
     while (1) {
-        DDRC = (1 << PC1);
+        DDRC &= ~(1 << PC1);
+
         ADCSRA |= (1 << ADSC);  // Start the conversion
 
         while (!(ADCSRA & (1 << ADIF)) == 1){;}   // Wait until the conversion is over
-        // while (!(ADCSRA & (1 << ADSC)) == 1){;}  // Wait until the conversion is over
         
-        uint16_t decimal_val = (ADCH << 2) | (ADCL >> 6); // Get the converted decimal value from combining ADCL and ADCH to get 10-bit ADC result
-
-        int voltage = (decimal_val * 8) / 1023;
-
-        usart_send(voltage);
+        unsigned int decimal_val = (ADCL >> 6) | (ADCH << 2); // Get the converted decimal value from combining ADCL and ADCH to get 10-bit ADC result
+       
+        unsigned int voltage = (decimal_val * 8) / 1023;
 
         PORTD = 0x00;  // Turn off all LEDs
 
         for (int i = 0; i < voltage; i++) {
             PORTD |= (1 << i); 
-            //_delay_ms(1000); // Turn on the calculated number of LEDs
         }
 
         _delay_ms(1000);
 
-        ADCH = 0x00;
-        ADCL = 0x00;
-        voltage = 0;
-        ADCSRA |= (1 << ADIF);
-
-          // Clear the ADIF flag
+        ADCSRA |= (1 << ADIF); // Clear the ADIF flag          
     }
 
     return 0;
--- Lab5/Task2.c
+++ Lab5/Task2.c
@@ -3,36 +3,13 @@
 #include <util/delay.h>
 
 void adc_init() {
-    DDRC &= ~(1 << PC0);
 
     ADCSRA |= (1 << ADEN);    // Enable the ADC
-    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADATE);    // Set 64 as the conversion speed
-    // ADCSRA |= (1 << ADATE);    
+    ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADATE);    // Set 64 as the conversion speed 
 
     ADMUX |= (1 << ADLAR);  // Set voltage reference as AVCC, left adjust result, and set ADC1 as the analog input channel
 
-    UBRR0 = 103;
-
-    // Enable USART transmitter and receiver
-    UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-
-    // 8-bit data, no parity, 1 stop bit
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
-
-void usart_send(uint16_t data) {    
-    while (!(UCSR0A & (1 << UDRE0)));   // Wait until the transmitter is ready
-    UDR0 = data;
-}
-
-// // transmit a string
-// void usart_send_string(int str) {
-//     // Transmit each character of the string
-//     while (str) {
-//         usart_send*str);
-//         str++;
-//     }
-// }
 
 int main(){
 
@@ -40,26 +17,26 @@ int main(){
 
     adc_init(); 
 
-
-
     while (1){
-    // PORTD = (1<<PD7);
+    DDRC &= ~(1 << PC0);
     
     ADCSRA |= (1 << ADSC);  // Start the conversion
 
     while (!(ADCSRA & (1 << ADIF)) == 1){;}   // Wait until the conversion is over
 
-    uint16_t decimal_val = (ADCH << 2) | (ADCL >> 6); // Get the converted decimal value from combining ADCL and ADCH to get 10-bit ADC result
-    uint16_t voltage = (decimal_val * 8) / 64;
+    unsigned int decimal_val = (ADCL >> 6) | (ADCH << 2); // Get the converted decimal value from combining ADCL and ADCH to get 10-bit ADC result
+    
+    unsigned int voltage = (decimal_val * 8) / 1023;
 
-    //print value to serial monitor
-    usart_send(voltage);
+    if (voltage < 4){
+        PORTD |= (1<<PD7);
+    }
 
-    ADCSRA |= (1 << ADIF);
+    if (voltage >= 4){
+        PORTD &= ~(1<<PD7);
+    }
 
-    ADCH = b00000000;
-    ADCL = b00000000;	
-
+    ADCSRA |= (1 << ADIF);	
     }
 
     return 0;
